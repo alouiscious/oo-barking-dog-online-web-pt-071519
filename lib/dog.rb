a547fb6a1248d2f70970731de46476fca9807a51
Renee Sims <info@reneesoul.com>
2019-08-06 06:14:47 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,27 @@
-# Your code goes here!
+class  Dog 
+  def name=(dog_name)
+    @dogs_name = dog_name
+  end
+  
+  def name
+    @dogs_name
+  end
+  
+  def bark=(dog_bark)
+    @dog_barks = "woof!"
+  end
+  
+  def bark
+    @dog_barks
+  end
+  
+end
+
+fido = Dog.new
+fido.name = "Fido"
+ 
+fido.name
+
+fido.bark
+
+  # Your code goes here!
